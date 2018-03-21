30c00e12ef17e2f655b865cfe323b85ccd06ab01
pmsabat <pasab75@gmail.com>
2018-03-21 12:09:20 +0000

    updating with pre-commit hooks and more boilerplate

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+    constructor(props){
+        super(props);
+
+    }
+    render(){
+        return (
+            <div>
+                <h2>hi</h2>
+                <p>this is the about page</p>
+            </div>
+        )
+    }
+}
