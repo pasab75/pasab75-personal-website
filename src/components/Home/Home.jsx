6868ba1f6d1f3fcdb1d7105cb36077abc49c8128
pmsabat <pasab75@gmail.com>
2018-03-21 01:14:23 +0000

    adding skeleton

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+    constructor(props){
+        super(props);
+
+    }
+    render(){
+        return (
+            <div>
+                <h2>hi</h2>
+                <p>this is the home page</p>
+            </div>
+        )
+    }
+}
