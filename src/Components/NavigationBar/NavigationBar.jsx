33a03e8daeffaf14fb1fc9a1eaaa5d31911d7bdd
pmsabat <pasab75@gmail.com>
2018-03-21 12:25:07 +0000

    test hooks

--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 import { Navbar, NavbarBrand, NavbarToggler } from "reactstrap";
 import "../../Static/bootswatch.scss";
 
-//Exports our bootstrap style bar
+// Exports our bootstrap style bar for use in the router
 export default class NavigationBar extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <h2>hi</h2>
-        <p>this is the home page</p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h2>hi</h2>
+                <p>this is the home page</p>
+            </div>
+        );
+    }
 }
