170dde0c2c9076a408a817a0b025cbe360742bf3
pmsabat <pasab75@gmail.com>
2018-03-21 12:12:18 +0000

    testing hooks

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Navbar, NavbarBrand, NavbarToggler } from "reactstrap";
 
 export default class Home extends Component {
-    constructor(props){
-        super(props);
-
-    }
-    render(){
-        return (
-            <div>
-                <h2>hi</h2>
-                <p>this is the home page</p>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>hi</h2>
+        <p>this is the home page</p>
+      </div>
+    );
+  }
 }
